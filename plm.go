e2f1b5a9eb1fd64c30b7f8fa3bddeecec77072c9
Joel Midstjärna <joel.midstjarna@gmail.com>
2018-03-25 17:50:07 +0200

    Refactoring

--- plm.go
+++ plm.go
@@ -1,10 +1,17 @@
 package main
-/*
+
 import (
 	"fmt"
+	"os"
+	"signal"
 )
-*/
+
 func main() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	fmt.Println("Please press CTRL+C")
+	sig <- c
+	fmt.Println("Bye bye")
 /*	configuration := LoadConfiguration(DefaultConfigFile)
 	m := CreateMeasurement(configuration.FastLogSize, configuration.SlowLogSize, 
 	                       sync.Mutex{}, proci.Proci{})
